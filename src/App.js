5b19c39cb648735c4583e4f683d2b3abc8006ac4
Developer-Devyansh <devyanshyadav41@gmail.com>
2023-09-05 16:44:37 +0530

    Git Changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,131 @@
-import React from 'react'
-import Cards from './components/Cards'
-import cardDetail from './components/CardsDetail'
-import "./App.css"
-import { ImgCard } from './components/Cards'
+import React, { useState } from "react";
+import "animate.css";
 
-const cards = cardDetail.map((elem) => (
-  <>
-    <Cards
-      title={elem.cardTitle}
-      event={elem.eventName}
-      link={elem.pageLink}
-      txtalign={elem.txtAlign}
-    />
-    <ImgCard image={elem.eventImage} />
-
-  </>
-))
 const App = () => {
+  const [formIn, setformIn] = useState({
+    email: "",
+    password: "",
+  });
+  const formInput = (event) => {
+    let name = event.target.name;
+    let value = event.target.value;
+    setformIn((prev) => {
+      if (name == "email") {
+        return {
+          email: value,
+          password: prev.password,
+        };
+      } else {
+        return {
+          email: prev.email,
+          password: value,
+        };
+      }
+    });
+  };
+
+  const [formOut, setformOut] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [style, setStyle] = useState({
+    display: "none",
+    color: "",
+    txt: "",
+  });
+  const formOutput = () => {
+    setformOut(() => {
+      return {
+        email: formIn.email,
+        password: formIn.password,
+      };
+    });
+
+    if (
+      formIn.email == "example@gmail.com" &&
+      formIn.password == "developerZone"
+    ) {
+      setStyle(() => {
+        return {
+          display: "block",
+          color: "#25ab61",
+          txt: " Login Successfully",
+        };
+      });
+    } else {
+      setStyle(() => {
+        return {
+          display: "block",
+          color: "red",
+          txt: "Incorrect Details",
+        };
+      });
+    }
+    setTimeout(() => {
+      setStyle(() => {
+        return {
+          display: "none",
+        };
+      });
+    }, 2000);
+  };
+
   return (
     <>
-      {cards}
-
+      <section className="formSection">
+        <div className="imgDiv">
+          <img src="https://kaiber.ai/images/8efc3d7b7027c26ee43eea2696966107-logo.png" />
+          <span>BETA</span>
+        </div>
+        <div className="inputFields">
+          <span>
+            <p>Email</p>
+            <input
+              type="email"
+              placeholder="Enter email address"
+              name="email"
+              onChange={formInput}
+            />
+          </span>
+          <span>
+            <p>Password</p>
+            <input
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              onChange={formInput}
+            />
+            <p className="forgetPass">Forgot Password?</p>
+          </span>
+        </div>
+        <div className="buttonsDiv">
+          <button onClick={formOutput}>Login</button>
+          <button>Sign Up</button>
+          <span>or</span>
+          <button>
+            <img
+              width="18"
+              height="18"
+              src="https://img.icons8.com/color/48/google-logo.png"
+              alt="google-logo"
+            />
+            Sign In With Google
+          </button>
+        </div>
+      </section>
+      <div
+        className="msgToUser animate__animated animate__bounceInRight"
+        style={{
+          display: style.display,
+          borderColor: style.color,
+          color: style.color,
+        }}
+      >
+        {style.txt}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
